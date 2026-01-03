5f461c1edf2162b4afd80ec7db535b14a15d4090
Adityarvs17 <adityarvs18@gmail.com>
2026-01-04 02:07:30 +0530

    Initial commit: LexiLens Dyslexia Assistant

---
+++ src/content/modifiers/index.ts
@@ -0,0 +1,11 @@
+// Modifiers barrel export
+
+export * from './typography';
+export * from './readingRuler';
+export * from './screenTint';
+export * from './focusMode';
+export * from './bionicReading';
+export * from './syllableSplitter';
+export * from './irisFocus';
+export * from './handFocus';
+
